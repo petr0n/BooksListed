5d131097663420b1cfd3e44008f0397b7e8885b3
petr0n <petron@gmail.com>
2019-03-13 19:34:13 -0500

    add ajax

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@ let getData = function(){
     let nyt_listParams = 'lists/2019-01-01/hardcover-fiction.json?';
     let nyt_key = 'api-key=jGJGGPp2j3xzgOmII98GpJxgukWxBnS3'
 
-    function getBookList(){
+    function getListNames(){
         let url = nyt_endpoint + 'lists/names.json?' + nyt_key;
         $.ajax({
             url: url
@@ -17,6 +17,27 @@ let getData = function(){
             console.log(itemList);
         });
     }
-    getBookList();
+    // getListNames();
+
+
+
+    function getBookList(listDate, listName) {
+        let date = listDate != '' ? listDate : '2019-01-01';
+        let name = listName != '' ? listName : 'hardcover-fiction';
+        let url = nyt_endpoint + 'lists/' + listDate + '/' + name + '.json?' + nyt_key;
+        $.ajax({
+            url: url
+        }).then(function(res){
+            let listTitle = res.results.list_name;
+            let books = res.results.books;
+            console.log(listTitle);
+            if (books) {
+                books.map(function(book){
+                    console.log(book.title);
+                });
+            }
+        });
+    }
+    getBookList('','');
 }
 getData();  
